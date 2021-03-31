7a1085c8a24b3cc98d57e70c9e50e629a76bef47
palomaraujo <palomaraujo4@gmail.com>
2021-03-31 18:41:24 -0300

    Inclusao do ErrorBoundry

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,6 +3,12 @@ import Card from './Card'
 
 const CardList = ({ robots }) => {
 
+    /*
+    if(true){
+        throw new Error('NOOOOOO');
+    }
+    */
+
     return (
         <div>
            {
